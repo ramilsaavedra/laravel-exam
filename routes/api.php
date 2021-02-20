ed74fb9812743bbb9a9ef8f248828fc23e7873ec
Ramil Saavedra <ramilsmcl@gmail.com>
2021-02-20 10:24:55 +0800

    edit readme

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('add-comment-via-command', 'CommentController@storeViaCommand')->name('store_comment_via_command');
+Route::post('api/add-comment-via-command', 'CommentController@storeViaCommand')->name('store_comment_via_command');
